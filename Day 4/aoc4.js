1e097d744230d45b7eaab155d2ba2f91e3261caa
nlewis84 <nathan@differential.com>
2021-12-04 13:35:29 -0600

    feat: added check for single board winner and winning combos

--- Day 4/aoc4.js
+++ Day 4/aoc4.js
@@ -5,9 +5,53 @@ const [bingoCaller, ...boardsInput] = fs
 	.split('\n\n')
   // .filter(e =>  e)
 
-console.log(bingoCaller)
-console.log(boardsInput)
+let boards = boardsInput.map(board => board
+  .split('\n')
+  .map(row => row
+    .trim()
+    .split(/\s+/)
+    .map(cell => parseInt(cell)))
+  .flat()
+  .map((item, i) => ({index: i, number: item, called: false})  
+  ));
 
-let boards = boardsInput.map(board => board.split('\n').map(row => row.trim().split(/\s+/).map(cell => parseInt(cell))));
+const WINNING_COMBINATIONS = [
+  [0, 1, 2, 3, 4],
+  [5, 6, 7, 8, 9],
+  [10, 11, 12, 13, 14],
+  [15, 16, 17, 18, 19],
+  [20, 21, 22, 23, 24],
+  [0, 5, 10, 15, 20],
+  [1, 6, 11, 16, 21],
+  [2, 7, 12, 17, 22],
+  [3, 8, 13, 18, 23],
+  [4, 9, 14, 19, 24]
+]
 
-console.log(boards[0].flat().map(item => ({number: item, called: false})))
+function checkSingleBoardForWinner(board) { 
+  let winner = false;
+  console.log(board);
+  
+  exit_loops:
+  for (let i = 0; i < WINNING_COMBINATIONS.length; i++) {
+
+    exit_combo:
+    for (let j = 0; j < WINNING_COMBINATIONS[i].length; j++) {
+      // console.log("2nd for: ", winner);
+      console.log(board[WINNING_COMBINATIONS[i][j]])
+      if (!board[WINNING_COMBINATIONS[i][j]].called) {
+        // console.log('if: ', winner);
+        winner = false;
+        break exit_combo;
+      }
+console.log("it's true!!!")
+      winner = true;
+      return winner;
+      break exit_loops;
+    }
+
+  }
+
+}
+
+checkSingleBoardForWinner(boards[0]);
