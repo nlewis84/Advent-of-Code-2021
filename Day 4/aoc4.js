db9048feba988862545d3b430240658ac9faf475
nlewis84 <nathan@differential.com>
2021-12-04 09:37:51 -0600

    chore: finished initial setup of Day 4, .env, and .gitignore

---
+++ Day 4/aoc4.js
@@ -0,0 +1,8 @@
+const { aoc_input } = require('../config');
+const fs = require('fs');
+const lines = fs
+	.readFileSync(`${aoc_input}`, 'utf-8')
+	.split('\n')
+  // .filter(e =>  e)
+
+  console.log(lines)
