b8aa2e6807dc6ae4f5085fa261ce244a26fb44f8
nlewis84 <nathan@differential.com>
2021-12-07 06:14:24 -0600

    feat: day 7 part 1 solution

--- Day 7/aoc7.js
+++ Day 7/aoc7.js
@@ -1,9 +1,37 @@
 const { aoc_input } = require('../config');
 const fs = require('fs');
-const lanternFishSchool = fs
+const crabs = fs
 	.readFileSync(`${aoc_input}`, 'utf-8')
 	.split(',')
-	.map((fish) => {
-		return parseInt(fish);
+	.map((num) => {
+		return parseInt(num);
 	});
 
+// console.log(crabs)
+
+let fuelCost = Number.MAX_VALUE;
+let min = Math.min(...crabs);
+let max = Math.max(...crabs);
+
+// console.log(min, max)
+
+function part1() { 
+	let bestPosition = 0;
+
+	for (let i = min; i <= max; i++) {
+		let requiredFuel = crabs.reduce((fuel, crabPosition) => {
+			// console.log("fuel", fuel, "position", crabPosition)
+			let necessaryFuelForCrab = Math.abs(i - crabPosition);
+			return fuel + necessaryFuelForCrab;
+		}, 0);
+
+		if (requiredFuel < fuelCost) {
+			fuelCost = requiredFuel;
+			bestPosition = i;
+		}
+	}
+	console.log({fuelCost, bestPosition})
+	return {fuelCost, bestPosition};
+}
+
+part1()
