5beb72c6d9eb3c1d61a72a3d759f754965db8d2b
nlewis84 <nathan@differential.com>
2021-12-08 06:22:37 -0600

    feat: working part 1 solution

--- Day 8/aoc8.js
+++ Day 8/aoc8.js
@@ -1,9 +1,35 @@
 const { aoc_input } = require('../config');
 const fs = require('fs');
-const crabs = fs
+const input = fs
 	.readFileSync(`${aoc_input}`, 'utf-8')
-	.split(',')
-	.map((num) => {
-		return parseInt(num);
-	});
+	.split(' | ')
+	.map((pattern) => {
+		return pattern.split('\n')
+	})
+	.flat()
+	.map((pattern) => {
+		return pattern.split(' ')
+	})
 
+// console.log(input)
+
+const signalPatterns = input.filter((pattern, i) => {
+	return i % 2 === 0;
+})
+
+// console.log(signalPatterns)
+
+const digitalOutput = input.filter((pattern, i) => {
+	return i % 2 === 1;
+})
+
+console.log(digitalOutput.flat())
+// 1, 4, 7 and 8 are the easy ones : 1 = 2 segments, 4 = 4seg, 7 = 3seg, 8 = 7seg
+
+const signalPatternsOrdered = signalPatterns.sort((a,b) => a.length - b.length);
+
+// console.log(signalPatternsOrdered)
+
+let sortedDigitalOutput = digitalOutput.flat().filter(item => item.length === 2 || item.length === 3 || item.length === 4 || item.length === 7)
+
+console.log(sortedDigitalOutput.length)
