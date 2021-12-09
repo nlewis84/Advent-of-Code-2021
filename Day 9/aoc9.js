28f0bb61d1496d3c159729ff44724372947d194c
nlewis84 <nathan@differential.com>
2021-12-09 06:36:33 -0600

    feat: solved part 1

--- Day 9/aoc9.js
+++ Day 9/aoc9.js
@@ -1,14 +1,65 @@
 const { aoc_input } = require('../config');
 const fs = require('fs');
-const heightMap = fs
-  .readFileSync(`${aoc_input}`, 'utf-8')
-  .split(' | ')
-  .map((pattern) => {
-    return pattern.split('\n');
-  })
-  .flat()
-  .map((pattern) => {
-    return pattern.split(' ');
-  });
+const heightMap = fs.readFileSync(`${aoc_input}`, 'utf-8').split('\n');
 
-console.log(heightMap);
+// const length = heightMap[0].length;
+// const newRow = () => {
+//   const newArr = [];
+
+//   for (let i = 0; i < length; i++) {
+//     newArr.push('9');
+//   }
+
+//   return newArr;
+// };
+// const newHeightMap = heightMap.unshift(newRow());
+
+// console.log(length, newHeightMap);
+
+function indexOfSmallest(arr) {
+  let lowest = 0;
+
+  if (!arr.every((val) => val === arr[0])) {
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < arr[lowest]) lowest = i;
+    }
+
+    return lowest;
+  } else {
+    return 3;
+  }
+}
+
+const lowPoints = () => {
+  let points = [];
+
+  for (let index = 0; index < heightMap.length; index++) {
+    const prevRow = heightMap[index - 1];
+    const row = heightMap[index];
+    const nextRow = heightMap[index + 1];
+
+    console.log(index, row);
+    for (let j = 0; j < row.length; j++) {
+      const leftHeight = row[j - 1] || '9';
+      const currentHeight = row[j];
+      const rightHeight = row[j + 1] || '9';
+      const upperHeight = prevRow ? prevRow[j] : '9';
+      const lowerHeight = nextRow ? nextRow[j] : '9';
+      const heights = [
+        currentHeight,
+        lowerHeight,
+        upperHeight,
+        leftHeight,
+        rightHeight,
+      ];
+
+      indexOfSmallest(heights) === 0
+        ? points.push(parseInt(currentHeight) + 1)
+        : null;
+    }
+    console.log(points);
+  }
+  console.log(points.reduce((a, b) => a + b, 0));
+};
+
+lowPoints();
